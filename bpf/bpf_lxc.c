1a427322a67e877a55b5abb94037953d285629ca
Quentin Monnet <quentin@isovalent.com>
2020-02-18 17:12:51 +0000

    [WIP] bpf: support IPv4 fragments for L4 load balancer key extraction
    Add support for IPv4 fragments for L4 load balancer.
    
    Define and use a LRU table for storing information about datagram
    fragments. Add the relevant processing functions to ipv4.h. When a
    TCP/UDP IPv4 fragment is found:
    
    - If this is the first (logical) fragment of the datagram, add an entry
      to that map using (dst_ip, src_ip, proto, datagram_id) as a key and
      (dst_port, src_port) as value.
    
    - If this is another fragment in the datagram, attempt to retrieve L4
      ports from that map.
    
    Fragment processing is added to `extract_l4_port()`, where L4 ports
    extraction is performed for load balancing, to avoid adding a new
    switch/case on ip4->protocol elsewhere. However, it is worth noting that
    `extract_l4_port()`, called by `lb(4|6)_extract_key()`, is also part of
    the `nodeport_lb(4|6)()` path, and I am unsure whether this is
    desirable.
    
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2016-2019 Authors of Cilium
+ *  Copyright (C) 2016-2020 Authors of Cilium
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -456,7 +456,7 @@ static inline int handle_ipv4_from_lxc(struct __ctx_buff *ctx, __u32 *dstID)
 
 	l4_off = l3_off + ipv4_hdrlen(ip4);
 
-	ret = lb4_extract_key(ctx, &tuple, l4_off, &key, &csum_off, CT_EGRESS);
+	ret = lb4_extract_key(ctx, ip4, l4_off, &key, &csum_off, CT_EGRESS);
 	if (IS_ERR(ret)) {
 		if (ret == DROP_UNKNOWN_L4)
 			goto skip_service_lookup;
